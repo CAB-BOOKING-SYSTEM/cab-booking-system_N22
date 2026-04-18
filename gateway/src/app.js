30a1610cacfc84d525a44579d519fe14d38ca28d
HoangKhang <nguyenhoangkhq7@gmail.com>
2026-04-18 16:33:48 +0700

    fix 3 warning gateway

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -5,14 +5,51 @@ const authMiddleware = require("./middlewares/auth");
 const app = express();
 app.use(express.json());
 
-app.use("/auth", proxy("http://cab_auth:${AUTH_PORT}"));
-app.use("/api/v1/users", authMiddleware, proxy("http://cab_user:${USER_PORT}"));
-app.use("/drivers", authMiddleware, proxy("http://cab_driver:${DRIVER_PORT}"));
-app.use("/bookings", authMiddleware, proxy("http://cab_booking:${BOOKING_PORT}"));
-app.use("/rides", authMiddleware, proxy("http://cab_ride:${RIDE_PORT}"));
-app.use("/pricing", authMiddleware, proxy("http://cab_pricing:${PRICING_PORT}"));
-app.use("/payments", authMiddleware, proxy("http://cab_payment:${PAYMENT_PORT}"));
-app.use("/notifications", authMiddleware, proxy("http://cab_notification:${NOTIFICATION_PORT}"));
-app.use("/reviews", authMiddleware, proxy("http://rb_preview:${REVIEW_PORT}"));
+const createProxy = (target, prefix) =>
+  proxy(target, {
+    proxyReqPathResolver: (req) => `${prefix}${req.url}`,
+  });
+
+app.use("/auth", createProxy("http://auth-service:3001", "/api/auth"));
+app.use(
+  "/api/v1/users",
+  authMiddleware,
+  createProxy("http://user-service:3009", "/api/v1/users"),
+);
+app.use(
+  "/drivers",
+  authMiddleware,
+  createProxy("http://driver-service:3003", "/api/drivers"),
+);
+app.use(
+  "/bookings",
+  authMiddleware,
+  createProxy("http://booking-service:3002", "/api"),
+);
+app.use(
+  "/rides",
+  authMiddleware,
+  createProxy("http://ride-service:3008", "/rides"),
+);
+app.use(
+  "/pricing",
+  authMiddleware,
+  createProxy("http://pricing-service:3006", "/api/v1"),
+);
+app.use(
+  "/payments",
+  authMiddleware,
+  createProxy("http://payment-service:3005", "/api/payments"),
+);
+app.use(
+  "/notifications",
+  authMiddleware,
+  createProxy("http://notification-service:3004", "/notifications"),
+);
+app.use(
+  "/reviews",
+  authMiddleware,
+  createProxy("http://review-service:3007", "/api/v1/reviews"),
+);
 
 module.exports = app;
